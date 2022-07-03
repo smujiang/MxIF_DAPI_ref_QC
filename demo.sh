927741f63d9053e1e2ef9145af28132153d43cf3
Jiang <jiang.jun@mayo.edu>
2022-07-03 10:37:19 -0500

    Create demo.sh

---
+++ demo.sh
@@ -0,0 +1,4 @@
+python3 run_qc.py  \
+-i /research/bsi/archive/PI/Goode_Ellen_m004290/tertiary/s302493.MxIF_Ovarian_Cancer/integrated/OVCA_TMA22_Pilot/OVCA_TMA22/RegisteredImages \
+-c OVCA_TMA22 \
+-o /research/bsi/projects/staff_analysis/m192500/MxIF_CellSeg/OME_TIFF/QC_out
