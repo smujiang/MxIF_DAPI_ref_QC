7507bbfd6f7a6821ae62654a77576728dd3fe2db
Jun Jiang <smujiang@gmail.com>
2022-07-07 09:55:13 -0500

    update path

--- demo.sh
+++ demo.sh
@@ -1,4 +1,4 @@
 python3 run_qc.py  \
--i /research/bsi/archive/PI/Goode_Ellen_m004290/tertiary/s302493.MxIF_Ovarian_Cancer/integrated/OVCA_TMA22_Pilot/OVCA_TMA22/RegisteredImages \
+-i /research/bsi/archive/PI/Goode_Ellen_m004290/tertiary/s302493.MxIF_Ovarian_Cancer/integrated/OVCA_TMA22_Pilot \
 -c OVCA_TMA22 \
 -o /research/bsi/projects/staff_analysis/m192500/MxIF_CellSeg/OME_TIFF/QC_out
